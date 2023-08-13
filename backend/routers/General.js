9d74ae544feac9ae4c9b1848c231a807718240c1
leula7 <leulkahssaye1000@gmail.com>
2023-08-13 13:01:54 +0300

    adding token as middelware on branch route

--- backend/routers/General.js
+++ backend/routers/General.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
-import {  ForgetPassword, TotalApprove, TotalBIdParticipants, TotalBranchApprovedRequests, TotalBranchPendingRequests, TotalBranchRequests, TotalPendings, TotalRequest, TotalRequests, TotalTask, UpdatePassword, branch, branchname, cats, items, oneitem } from '../controller/general.conntroller.js';
+import {  ForgetPassword, TotalApprove, TotalBIdParticipants, TotalBranchApprovedRequests,
+   TotalBranchPendingRequests, TotalBranchRequests, TotalPendings, TotalRequest,
+    TotalRequests, TotalTask, UpdatePassword, branch, branchname, cats, items, oneitem } from '../controller/general.conntroller.js';
+import verifyToken from '../verifyToken.js';
 
 const router = Router();
 
-  router.get('/branch',branch);
+  router.get('/branch',verifyToken,branch);
 
   router.post('/forgetpass',ForgetPassword);
 
